a6605703e42be2f1b72e61884e209d2c0d1e55e9
Theodore Pollock <pollock86@yahoo.com>
2020-12-07 17:23:25 -0600

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    //TODO - 1) Ask user for name, string
+    //TODO - 2) Ask user for how many in party, int
+    //TODO - 3) Track number of party members, int
+    //TODO - 4) Print out guest list, and total guest numbers, string and int
+    class Program
+    {
+        public static string fullName;
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the party guest list.");
+
+            Console.ReadLine();
+        }
+    }
+}
